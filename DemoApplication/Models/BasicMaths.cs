2dcfc57f9de9ba3df650cd26c5459427cbaf0c97
Prafull Pandey <prafull_pandey1@yahoo.com>
2019-10-10 04:28:51 +0000

    multiplication operator correction

--- DemoApplication/Models/BasicMaths.cs
+++ DemoApplication/Models/BasicMaths.cs
@@ -22,7 +22,7 @@ namespace DemoApplication.Models
         public double Multiply(double num1, double num2)
         {
             // To trace error while testing, writing + operator instead of * operator.  
-            return num1 + num2;
+            return num1 * num2;
         }
     }
 }
